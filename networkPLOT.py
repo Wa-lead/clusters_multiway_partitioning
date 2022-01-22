03567743beaecfd1d10c23386790370ba6a171b6
Wa-lead <waleedaaofficial@gmail.com>
2022-01-22 21:35:32 +0300

    two way partitioning

--- networkPLOT.py
+++ networkPLOT.py
@@ -9,6 +9,8 @@ import scipy.stats
 import matplotlib.pyplot as plt
 
 
+fig, ax = plt.subplots(2, figsize=(8, 14))
+
 array_database = []
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -21,7 +23,7 @@ yDelta=yMax-yMin; #rectangle dimensions
 areaTotal=xDelta*yDelta
  
 #Point process parameters
-lambda0=0.4; #intensity (ie mean density) of the Poisson process
+lambda0=1.5; #intensity (ie mean density) of the Poisson process
  
 #Simulate Poisson point process
 numbPoints = (scipy.stats.poisson( lambda0*areaTotal ).rvs())*2#Poisson number of points # multiply by 2 to make it always even
@@ -31,14 +33,14 @@ y = yDelta*scipy.stats.uniform.rvs(0,1,((numbPoints,1)))+yMin#y coordinates of P
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                                                             #Preparing The data point data base
 
+# create the two subsets A and B
+subsetA = [] #A
+subsetB = [] #B
+
 #create dictionary - associative array  - that holds all points as objects, the infected attribute is random (1= infected, 0= healthy)
-
-subsetA = []
-subsetB = []
-
 for i in range(numbPoints):
     array_database.append({ "index": i, "x":x[i][0], "y":y[i][0], "infected": 1 if random.randint(0, 9)>8 else 0, "Dvalue": 0})
-    if(i%2 == 0):
+    if(x[i][0]>=7 and len(subsetA)<numbPoints/2) or (len(subsetB) == numbPoints/2):
         subsetA.append(array_database[i])
     else:
         subsetB.append(array_database[i])
@@ -63,68 +65,134 @@ for index_first_point in range(numbPoints):#x
                                                             #Why? because we want to assign cost to each edge
 
 array_database= sorted(array_database, key=lambda x:x['index']) ## sort it based on index to uilize the edge matrix
-edgeMatrix = []
+edgeMatrix = [[0 for i in range(len(array_database))] for j in range(len(array_database))]
+
 
 for index_first_point in range(numbPoints):#x
-    pointEdges = [0]*numbPoints
+    index_second_point = index_first_point +1
     for index_second_point in range(numbPoints):
         point1= array_database[index_first_point]
         point2= array_database[index_second_point]
         distance = sqrt((point1['x']-point2['x'])**2+(point1['y']-point2['y'])**2)
-        pointEdges[index_second_point] = 0 if index_second_point == index_first_point else distance
-    edgeMatrix.append(pointEdges)
+        edgeMatrix[index_first_point][index_second_point] = distance if distance <= 1 else 0
+        edgeMatrix[index_second_point][index_first_point] = edgeMatrix[index_first_point][index_second_point]
+
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-                                                            #Calculating the the D value
-for index1 in range(len(subsetA)):
-    ineternalCost = 0
-    externalCost = 0
-    for index2 in range(len(subsetA)): ## length of both subsets are the same
-        ineternalCost += edgeMatrix[subsetA[index1]['index']][subsetA[index2]['index']]
-        externalCost += edgeMatrix[subsetA[index1]['index']][subsetB[index2]['index']]
-    subsetA[index1]['Dvalue'] = externalCost - ineternalCost
-    
+                                                            #Plot the girst graph before partioning
+for index in range(numbPoints):
+    color='black'
+    if array_database[index]['x']>12 or array_database[index]['x']<2 or array_database[index]['y']>12 or array_database[index]['y']<2:
+        color='grey'
+    elif array_database[index] in subsetA:
+        color='green'
+    else:
+        color='purple'
 
-for index1 in range(len(subsetB)):
-    ineternalCost = 0
-    externalCost =0
-    for index2 in range(len(subsetB)): ## length of both subsets are the same
-        ineternalCost += edgeMatrix[subsetB[index1]['index']][subsetB[index2]['index']]
-        externalCost += edgeMatrix[subsetB[index1]['index']][subsetA[index2]['index']]
-    subsetB[index1]['Dvalue'] = externalCost - ineternalCost
+    ax[0].scatter(array_database[index]['x'],array_database[index]['y'], c= color)
 
-subsetA= sorted(subsetA, key=lambda x:x['Dvalue'], reverse=True)
-subsetB= sorted(subsetB, key=lambda x:x['Dvalue'], reverse=True)
-G = 0
+for index_first_point in range(numbPoints):
+    for index_second_point in range(numbPoints):
+        point1= array_database[index_first_point]
+        point2= array_database[index_second_point]
+        if edgeMatrix[index_first_point][index_second_point] != 0: # the initial assumption is that only nodes within 1 kilometers of each other can connect
+            pointX = [point1['x'], point2['x']]
+            pointY = [point1['y'], point2['y']]
+            isInSubsetA= point1 in subsetA
+            ax[0].plot(pointX, pointY,'green' if isInSubsetA  else 'purple')
 
-Xstar = []
-Ystar = []
-for i in range(len(subsetA)):
-    gain = -10000
-    indexOfa1 = 0
-    indexOfb1 = 0
-    for o in range(len(subsetA)):
-        for k in range(len(subsetA)): #Finding the maximum gain resulted from exchange
-            newGain=subsetA[o]['Dvalue'] + subsetB[k]['Dvalue'] - 2*edgeMatrix[subsetA[o]['index']][subsetB[k]['index']]
-            if newGain > gain:
-                gain = newGain
-                indexOfb1 = k
-                indexOfa1 = o
-    G+= gain    
-    for j in range(len(subsetA)):#Recalculating the Dvalue of each in the session
-        subsetA[j]['Dvalue'] = subsetA[j]['Dvalue'] +  2*edgeMatrix[subsetA[j]['index']][subsetA[indexOfa1]['index']] - 2*edgeMatrix[subsetA[j]['index']][subsetB[indexOfb1]['index']]
-        subsetB[j]['Dvalue'] = subsetB[j]['Dvalue'] +  2*edgeMatrix[subsetB[j]['index']][subsetB[indexOfb1]['index']] - 2*edgeMatrix[subsetB[j]['index']][subsetA[indexOfa1]['index']]
-    #excluding the interchanged values
-    Xstar.append(subsetA.pop(indexOfa1))
-    Ystar.append(subsetB.pop(indexOfb1))
+ax[0].plot([2,2], [2,12],color='black')
+ax[0].plot([2,12], [12,12],color='black')
+ax[0].plot([12,12], [12,2],color='black')
+ax[0].plot([12,2], [2,2], color='black')
 
-    #sorting based on Dvalue again
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                          #Calculating the the D value
+
+while True:
+    for index1 in range(len(subsetA)):
+        ineternalCost = 0
+        externalCost = 0
+        for index2 in range(len(subsetA)): ## length of both subsets are the same
+            ineternalCost += edgeMatrix[subsetA[index1]['index']][subsetA[index2]['index']]
+            externalCost += edgeMatrix[subsetA[index1]['index']][subsetB[index2]['index']]
+        subsetA[index1]['Dvalue'] = externalCost - ineternalCost
+        
+
+    for index1 in range(len(subsetB)):
+        ineternalCost = 0
+        externalCost =0
+        for index2 in range(len(subsetB)): ## length of both subsets are the same
+            ineternalCost += edgeMatrix[subsetB[index1]['index']][subsetB[index2]['index']]
+            externalCost += edgeMatrix[subsetB[index1]['index']][subsetA[index2]['index']]
+        subsetB[index1]['Dvalue'] = externalCost - ineternalCost
+
     subsetA= sorted(subsetA, key=lambda x:x['Dvalue'], reverse=True)
     subsetB= sorted(subsetB, key=lambda x:x['Dvalue'], reverse=True)
 
-subsetA.extend(Ystar)
-subsetB.extend(Xstar)
+    #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-print (int(G))
+    G = 0
+
+    Xstar = []
+    Ystar = []
+    GBuffer = []
+
+    #copies are used to maintain the state of the original sets
+    subsetACopy = subsetA.copy()
+    subsetBCopy = subsetB.copy()
+
+    for i in range(len(subsetACopy)):
+        gain = -10000
+        indexOfa1 = 0
+        indexOfb1 = 0
+        #find the highest gain
+        for j in range(len(subsetACopy)):
+            for k in range(len(subsetACopy)):
+                newGain = subsetACopy[j]['Dvalue'] + subsetBCopy[k]['Dvalue'] - 2*edgeMatrix[subsetACopy[j]['index']][subsetBCopy[k]['index']]
+                if newGain > gain:
+                    gain = newGain
+                    indexOfa1 = j
+                    indexOfb1 = k
+        
+        G+= gain
+    
+        #Recalculating the Dvalue of each in the session
+        for j in range(len(subsetACopy)):
+            subsetACopy[j]['Dvalue'] = subsetACopy[j]['Dvalue'] +  2*edgeMatrix[subsetACopy[j]['index']][subsetACopy[indexOfa1]['index']] - 2*edgeMatrix[subsetACopy[j]['index']][subsetBCopy[indexOfb1]['index']]
+            subsetBCopy[j]['Dvalue'] = subsetBCopy[j]['Dvalue'] +  2*edgeMatrix[subsetBCopy[j]['index']][subsetBCopy[indexOfb1]['index']] - 2*edgeMatrix[subsetBCopy[j]['index']][subsetACopy[indexOfa1]['index']]
+        
+        ##------------------------------------------- the mistake is here, fix the popping problem
+
+
+        #remove the highest gain points from the set
+        Xstar.append(subsetACopy.pop(indexOfa1))
+        Ystar.append(subsetBCopy.pop(indexOfb1))
+
+
+        #buffers the gain 
+        GBuffer.append({"index" :i, "G": G, "Xstar": Xstar.copy(), "Ystar": Ystar.copy()})
+
+        #sorting based on Dvalue again
+        subsetACopy= sorted(subsetACopy, key=lambda x:x['Dvalue'], reverse=True)
+        subsetBCopy= sorted(subsetBCopy, key=lambda x:x['Dvalue'], reverse=True)
+
+    #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #remove highest gain exchanges from the subsetA and subsetB
+    GBuffer= sorted(GBuffer, key=lambda x:x['G'], reverse=True)
+
+    if(GBuffer[0]['G']<=0.000000000001):
+        break
+
+    for point in GBuffer[0]['Xstar']:
+        subsetA.remove(point)
+
+    for point in GBuffer[0]['Ystar']:
+        subsetB.remove(point)   
+
+    #add the interchanged points to their new sets
+    subsetA = subsetA + GBuffer[0]['Ystar']
+    subsetB = subsetB + GBuffer[0]['Xstar']
+
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                                                             #Plotting the points on graph
 
@@ -138,26 +206,27 @@ for index in range(numbPoints):
     else:
         color='purple'
 
-    plt.scatter(array_database[index]['x'],array_database[index]['y'], c= color)
+    ax[1].scatter(array_database[index]['x'],array_database[index]['y'], c= color)
 
 
 
-#iterates through the the array and connects every node within distance 1 kilometer
+# iterates through the the array and connects every node within distance 1 kilometer
 for index_first_point in range(numbPoints):
     for index_second_point in range(numbPoints):
         point1= array_database[index_first_point]
         point2= array_database[index_second_point]
-        if edgeMatrix[index_first_point][index_second_point] <= 1: # the initial assumption is that only nodes within 1 kilometers of each other can connect
+        if edgeMatrix[index_first_point][index_second_point] != 0: # the initial assumption is that only nodes within 1 kilometers of each other can connect
             pointX = [point1['x'], point2['x']]
             pointY = [point1['y'], point2['y']]
-            plt.plot(pointX, pointY, color='black')
+            isInSubsetA= point1 in subsetA
+            ax[1].plot(pointX, pointY,'green' if isInSubsetA  else 'purple')
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                                                             #Drawing the boundries of the range
-plt.plot([2,2], [2,12],color='black')
-plt.plot([2,12], [12,12],color='black')
-plt.plot([12,12], [12,2],color='black')
-plt.plot([12,2], [2,2], color='black')
+ax[1].plot([2,2], [2,12],color='black')
+ax[1].plot([2,12], [12,12],color='black')
+ax[1].plot([12,12], [12,2],color='black')
+ax[1].plot([12,2], [2,2], color='black')
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
